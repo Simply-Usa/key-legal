3c964222e33c7b90c2c2f03c4f0bf0863d383fc8
Simply-Usa <50599349+Simply-Usa@users.noreply.github.com>
2019-09-25 13:24:04 +0600

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,3 +17,56 @@ $(window).scroll(function(){
 		menu.removeClass('fixed');
 	}
 });
+
+//tabs
+$(".tabs-item").not(":first").hide();
+$('#tabs_container .tabs li').click(function() {
+	$('#tabs_container .tabs li').removeClass("active").eq($(this).index()).addClass("active");
+	$('.tabs-item').hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass('active');
+
+//sldiers
+var left = 0;
+var leftTwo = 0;
+function slideLeft() {
+	var polosa = $('.slider_one .polosa')[0];
+	left -= 190;
+	if(left < -480){
+		left = 0;
+	}
+	polosa.style.left = left + 'px';
+}
+function slideRight() {
+	var polosa = $('.slider_one .polosa')[0];
+	left += 190;
+	if(left >= 0){
+		left = 0;
+	}
+	polosa.style.left = left + 'px';
+}
+//second slider
+function slideRightTwo(){
+	var polosa2 = $('.slider_two .polosa')[0];
+	leftTwo += 190;
+	if(leftTwo >= 0){
+		leftTwo = 0;
+	}
+	polosa2.style.left = leftTwo + 'px';
+}
+function slideLeftTwo(){
+	var polosa2 = $('.slider_two .polosa')[0];
+	leftTwo -= 190;
+	if(leftTwo < -480){
+		leftTwo = 0;
+	}
+	polosa2.style.left = leftTwo + 'px';
+}
+$('#to_right').click(slideLeft);
+$('#to_left').click(slideRight);
+$('#to_right__two').click(slideLeftTwo);
+$('#to_left__two').click(slideRightTwo);
+
+//questions accordion
+$('.questions > .question > .question_title').click(function(){
+	$(this).parents('.question').find('p, hr').slideToggle(200);
+})
